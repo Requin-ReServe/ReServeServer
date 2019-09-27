5b2d0b79f0e1e76dffce459e7e1c546c04b7ba9c
Maly <migsking@naver.com>
2019-09-27 13:45:05 +0900

    MARKET LIST 드디어 고침 ㅅㅂ
    ㄱㅇㄷ

--- views/api/service/market_list.py
+++ views/api/service/market_list.py
@@ -31,6 +31,7 @@ class MarketList(Resource):
 
     def post(self):
         market_name = list(request.json['market'].split())
+        print(market_name)
 
         # for i in market_name:
         #     test = Market_Model.objects.search_text(i).order_by('$text_score')
@@ -41,10 +42,11 @@ class MarketList(Resource):
         find = list()
         market_list = dict()
         name_markets_list = list()
-        a = 0
-        b = 0
-        flag = False
+        real_markets_list = list()
         return_market_list = list()
+        a = 0
+        flag = False
+        flag_num = 1
 
         for m in market_name:
             find.append(re.compile('.*'+m+'.*'))
@@ -65,41 +67,50 @@ class MarketList(Resource):
                     }
                     a += 1
 
+            return market_list, 201
         else:
             for i in find:
-                if find[b] is not None:
-                    if not flag:
-                        name_markets_list.append(Market_Model.objects(location = i).all())
-                        print('LOCATION', name_markets_list[b])
+                if not flag:
+                    name_markets_list.append(Market_Model.objects(location = i).all())
+                    flag_num += 1
+                    print('I GOT LOCATION')
+                    print('LOCATION LIST:', name_markets_list)
 
-                    if name_markets_list[b] is None:
-                        name_markets_list.append(Market_Model.objects(name = i).all())
-                        print('NAME', name_markets_list[b])
-                        break
+                if not name_markets_list[0]:
+                    del name_markets_list[0]
+                    name_markets_list.append(Market_Model.objects(name = i).all())
+                    flag_num += 1
+                    print('I GOT NAME')
+                    print('NAME LIST :',name_markets_list)
 
-                    if flag:
-                        name_markets_list.append(Market_Model.objects(name=i).all())
-                        print('TWO', name_markets_list[b])
+                if flag:
+                    print('I GOT FLAG')
+                    print(flag_num)
+                    if flag_num == 2:
+                        for j in name_markets_list[0]:
+                            real_markets_list.append(Market_Model.objects(Q(market_id = j['market_id']) & Q(name=i)).all())
+                            print("market_id :",j['market_id'])
+                            print(real_markets_list)
 
-                    flag = True
-                b += 1
+                    if flag_num == 3:
+                        for j in name_markets_list[0]:
+                            real_markets_list.append(Market_Model.objects(Q(market_id = j['market_id']) & Q(location=i)).all())
+                            print("market_id :",j['market_id'])
+                            print(real_markets_list)
 
-            for j in name_markets_list:
-                for k in j:
-                    for l in j:
-                        print(k['market_id'])
-                        if l['market_id'] == k['market_id']:
-                            print('SAM!')
-                            return_market_list.append(l)
+                flag = True
 
+            for i in real_markets_list:
+                for j in i:
+                    print(j['name'])
 
-            for market in return_market_list:
-                market_list[a] = {
-                    "name": market['name'],
-                    "location": market['location'],
-                    "phone": market['telephone_num']
-                }
+            for return_market_list in real_markets_list:
+                for market in return_market_list:
+                    market_list[a] = {
+                        "name": market['name'],
+                        "location": market['location'],
+                        "phone": market['telephone_num']
+                    }
 
-                a += 1
-            print(name_markets_list)
+                    a += 1
         return market_list, 201
