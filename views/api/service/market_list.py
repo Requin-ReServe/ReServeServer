ff637771f95ced245996f67cddbfa6ef5b9ea0e8
Maly <migsking@naver.com>
2019-09-27 09:36:37 +0900

    Update Market List 진짜 모르겠다

--- views/api/service/market_list.py
+++ views/api/service/market_list.py
@@ -38,35 +38,68 @@ class MarketList(Resource):
         #     for testing in test:
         #         print(testing['name'])
 
-        find = []
+        find = list()
+        market_list = dict()
+        name_markets_list = list()
+        a = 0
+        b = 0
+        flag = False
+        return_market_list = list()
 
         for m in market_name:
             find.append(re.compile('.*'+m+'.*'))
 
         try:
-            a = find[1]
+            find[1]
         except:
-                for i in find:
-                    name_markets = Market_Model.objects(Q(name=i) | Q(location=i)).all()
-                    for j in name_markets:
-                        print(j['name'])
+            flag = True
 
-        for i in find:
-            name_markets = Market_Model.objects(Q(name = i) & Q(location = i)).all()
-            for j in name_markets:
-                print(j['name'])
-        #
-        #     market_list = {}
-        #
-        #     a = 0
-        #
-        #     for market in name_markets:
-        #         print(market['name'])
-        #         market_list[a] = {
-        #             "name": market['name'],
-        #             "location": market['location'],
-        #             "phone": market['telephone_num']
-        #         }
-        #         a += 1
-        #
-        # return market_list, 201
+        if flag:
+            for i in find:
+                name_markets = Market_Model.objects(Q(name=i) | Q(location=i)).all()
+                for market in name_markets:
+                    market_list[a] = {
+                        "name": market['name'],
+                        "location": market['location'],
+                        "phone": market['telephone_num']
+                    }
+                    a += 1
+
+        else:
+            for i in find:
+                if find[b] is not None:
+                    if not flag:
+                        name_markets_list.append(Market_Model.objects(location = i).all())
+                        print('LOCATION', name_markets_list[b])
+
+                    if name_markets_list[b] is None:
+                        name_markets_list.append(Market_Model.objects(name = i).all())
+                        print('NAME', name_markets_list[b])
+                        break
+
+                    if flag:
+                        name_markets_list.append(Market_Model.objects(name=i).all())
+                        print('TWO', name_markets_list[b])
+
+                    flag = True
+                b += 1
+
+            for j in name_markets_list:
+                for k in j:
+                    for l in j:
+                        print(k['market_id'])
+                        if l['market_id'] == k['market_id']:
+                            print('SAM!')
+                            return_market_list.append(l)
+
+
+            for market in return_market_list:
+                market_list[a] = {
+                    "name": market['name'],
+                    "location": market['location'],
+                    "phone": market['telephone_num']
+                }
+
+                a += 1
+            print(name_markets_list)
+        return market_list, 201
